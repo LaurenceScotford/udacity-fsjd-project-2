a5e3cc31a3e516a9e947c0a0cbfa440661484a89
Laurence Scotford <laurencescotford@gmail.com>
2022-05-12 19:25:27 +0100

    Add url and description to product schema

--- migrations/sqls/20220226094440-products-up.sql
+++ migrations/sqls/20220226094440-products-up.sql
@@ -2,5 +2,7 @@ CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     price NUMERIC(12, 2),
+    url VARCHAR,
+    description VARCHAR,
     category bigint REFERENCES categories(id)
 );
